823a947a0026018357930e664c7e9d8956bff0a1
Van Anh <vananh3510@gmail.com>
2021-11-05 21:12:48 +0700

    Variables in Java

---
+++ src/lesson1/VariableLearning.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class VariableLearning {
+    public static void main(String [] args) {
+        int maHocSinh = 10;
+        float diemToan = 4.5f;
+        float diemVan = 5.5f;
+        float diemTiengAnh = 6.0f;
+        float diemTrungBinh = (diemToan * 2 + diemVan * 2 + diemTiengAnh) / 5;
+        boolean xetDiem = diemTrungBinh >= 5f;
+        System.out.println("Mã học sinh: " + maHocSinh);
+        System.out.printf("Điểm trung bình cộng của học sinh có mã số %d là: %.1f%n", maHocSinh, diemTrungBinh);
+        System.out.println("Học sinh có đủ điểm yêu cầu hay không? " +  xetDiem);
+    }
+}
